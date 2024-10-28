82fa1a309a85a4601dc46c8471a8f1e3aad28571
thuandang2022 <thuan.dang.cit22@gmail.com>
2024-10-27 23:55:18 -0700

    Thuan13h55
    Complete GetMapping whole *.html

--- project_A_intelliJ/src/main/java/com/example/project_a/Controllers/MainController.java
+++ project_A_intelliJ/src/main/java/com/example/project_a/Controllers/MainController.java
@@ -30,5 +30,43 @@ public class MainController {
     @GetMapping("/product-details.html")
     public String showProductDetails() {return "product-details.html";}
 
+    @GetMapping("/404.html")
+    public String showPageNotFound() {return "404.html";}
+
+    @GetMapping("/account.html")
+    public String showAccount() {return "account.html";}
+
+    @GetMapping("/blog.html")
+    public String showBlog() {return "blog.html";}
+
+    @GetMapping("/blog-details-audio.html")
+    public String showBlogDetailsAudio() {return "blog-details-audio.html";}
+
+    @GetMapping("/blog-details.html")
+    public String showBlogDetails() {return "blog-details.html";}
+
+    @GetMapping("/blog-details-blockquote.html")
+    public String showblogdetailsblockquote() {return "blog-details-blockquote.html";}
+
+    @GetMapping("/cart.html")
+    public String showCart() {return "cart.html";}
+
+    @GetMapping("/checkout.html")
+    public String showCheckout() {return "checkout.html";}
+
+    @GetMapping("/compare.html")
+    public String showCompare() {return "compare.html";}
+
+    @GetMapping("/forgot-password.html")
+    public String ShowPageForgotPassword() {return "forgot-password.html";}
+
+    @GetMapping("/login.html")
+    public String ShowPageLogin() {return "login.html";}
+
+    @GetMapping("/register.html")
+    public String ShowPageRegister() {return "register.html";}
+
+    @GetMapping("/wishlist.html")
+    public String ShowPageWishlist() {return "wishlist.html";}
 
 }
