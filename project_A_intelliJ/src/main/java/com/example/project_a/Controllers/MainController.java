fd5e65d1c7477e980ae8c1d4b80b662bfa4a23c1
vyvy77 <haingan77su@gmail.com>
2024-11-17 22:21:33 +0700

    unlock

--- project_A_intelliJ/src/main/java/com/example/project_a/Controllers/MainController.java
+++ project_A_intelliJ/src/main/java/com/example/project_a/Controllers/MainController.java
@@ -9,58 +9,58 @@ public class MainController {
     @GetMapping("")
     public String showHomePage() {return "index-2.html";}
 
-    @GetMapping("/HOME")
+    @GetMapping("/home")
     public String showIndexPage() {return "index-2.html";}
 
-//    @GetMapping("/shop-list.html")
-//    public String showProductPage() {return "shop-list.html";}
-//
-    @GetMapping("/blog.html")
+    @GetMapping("/shop")
+    public String showProductPage() {return "shop-list.html";}
+
+    @GetMapping("/blog")
     public String showBlogPage() {return "blog.html";}
-//
-//    @GetMapping("/contact.html")
-//    public String showContactPage() {return "contact.html";}
-//
-//    @GetMapping("/about.html")
-//    public String showAboutUsPage() {return "about.html";}
-//
-//    @GetMapping("/product-details.html")
-//    public String showProductDetails() {return "product-details.html";}
-//
-//    @GetMapping("/404.html")
-//    public String showPageNotFound() {return "404.html";}
-//
-//    @GetMapping("/account.html")
-//    public String showAccount() {return "account.html";}
-//
-//    @GetMapping("/blog-details-audio.html")
-//    public String showBlogDetailsAudio() {return "blog-details-audio.html";}
-//
-//    @GetMapping("/blog-details.html")
-//    public String showBlogDetails() {return "blog-details.html";}
-//
-//    @GetMapping("/blog-details-blockquote.html")
-//    public String showblogdetailsblockquote() {return "blog-details-blockquote.html";}
-//
-//    @GetMapping("/cart.html")
-//    public String showCart() {return "cart.html";}
-//
-//    @GetMapping("/checkout.html")
-//    public String showCheckout() {return "checkout.html";}
-//
-//    @GetMapping("/compare.html")
-//    public String showCompare() {return "compare.html";}
-//
-//    @GetMapping("/forgot-password.html")
-//    public String ShowPageForgotPassword() {return "forgot-password.html";}
-//
-//    @GetMapping("/login.html")
-//    public String ShowPageLogin() {return "login.html";}
-//
-//    @GetMapping("/register.html")
-//    public String ShowPageRegister() {return "register.html";}
-//
-//    @GetMapping("/wishlist.html")
-//    public String ShowPageWishlist() {return "/wishlist.html";}
+
+    @GetMapping("/contact")
+    public String showContactPage() {return "contact.html";}
+
+    @GetMapping("/about")
+    public String showAboutUsPage() {return "about.html";}
+
+    @GetMapping("/product-details")
+    public String showProductDetails() {return "product-details.html";}
+
+    @GetMapping("/404")
+    public String showPageNotFound() {return "404.html";}
+
+    @GetMapping("/account")
+    public String showAccount() {return "account.html";}
+
+    @GetMapping("/blog-details-audio")
+    public String showBlogDetailsAudio() {return "blog-details-audio.html";}
+
+    @GetMapping("/blog-details")
+    public String showBlogDetails() {return "blog-details.html";}
+
+    @GetMapping("/blog-details-blockquote")
+    public String showblogdetailsblockquote() {return "blog-details-blockquote.html";}
+
+    @GetMapping("/cart")
+    public String showCart() {return "cart.html";}
+
+    @GetMapping("/checkout")
+    public String showCheckout() {return "checkout.html";}
+
+    @GetMapping("/compare")
+    public String showCompare() {return "compare.html";}
+
+    @GetMapping("/forgot-password")
+    public String ShowPageForgotPassword() {return "forgot-password.html";}
+
+    @GetMapping("/login")
+    public String ShowPageLogin() {return "login.html";}
+
+    @GetMapping("/register")
+    public String ShowPageRegister() {return "register.html";}
+
+    @GetMapping("/wishlist")
+    public String ShowPageWishlist() {return "/wishlist.html";}
 
 }
