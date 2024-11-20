090f7c99f8092767dd4b56f875bc71749f9f2228
nqminh4604 <153005744+nqminh4604@users.noreply.github.com>
2024-11-21 00:40:31 +0700

    Change to JpaReposity (UserRepo)

--- project_A_intelliJ/src/main/java/com/example/project_a/user/UserRepository.java
+++ project_A_intelliJ/src/main/java/com/example/project_a/user/UserRepository.java
@@ -1,7 +1,8 @@
 package com.example.project_a.user;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
